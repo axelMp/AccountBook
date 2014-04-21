487b2600272764342529b9040071ca8464a6b84e
axelMp <axel.krauth@methodpark.de>
2014-04-21 21:33:07 +0200

    AJAX works now

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -103,18 +103,18 @@ environments {
 
 // log4j configuration
 log4j = {
-	appenders {
-			console name: 'stdout'
-		}
+	//appenders {
+	//		console name: 'stdout'
+	//	}
 		
-	all    'grails.app',
-	         'org.codehaus.groovy.grails.web.mapping'
+	//all    'grails.app',
+	//         'org.codehaus.groovy.grails.web.mapping'
 	
     error  'org.codehaus.groovy.grails.web.servlet',        // controllers
-           //'org.codehaus.groovy.grails.web.pages',          // GSP
-           //'org.codehaus.groovy.grails.web.sitemesh',       // layouts
-           //'org.codehaus.groovy.grails.web.mapping.filter', // URL mapping
-           //'org.codehaus.groovy.grails.web.mapping',        // URL mapping
+           'org.codehaus.groovy.grails.web.pages',          // GSP
+           'org.codehaus.groovy.grails.web.sitemesh',       // layouts
+           'org.codehaus.groovy.grails.web.mapping.filter', // URL mapping
+           'org.codehaus.groovy.grails.web.mapping',        // URL mapping
            'org.codehaus.groovy.grails.commons',            // core / classloading
            'org.codehaus.groovy.grails.plugins',            // plugins
            'org.codehaus.groovy.grails.orm.hibernate',      // hibernate integration
