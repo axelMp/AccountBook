80538ef47b7f951e312b7758c616d3cf128b767e
axelMp <axel.krauth@methodpark.de>
2014-04-23 08:04:25 +0200

    experiment with javascript and DOM

--- web-app/js/budgetPerAccount.js
+++ web-app/js/budgetPerAccount.js
@@ -1,3 +1,31 @@
+function visualizeAsTextField(nameOfField, text) {
+   return "<input type=\"text\" name=\""+nameOfField+"\" value=\""+text+"\">";
+}
+
+function visualizeDate(name,dateD) {
+	return "<input type=\"text\" name=\""+name+"\" value=\""+dateD+"\">";
+}
+
+function visualizeScheduleForEditing(aPlannedTransaction) {
+	return "from "+visualizeDate("startsOn",aPlannedTransaction.startsOn) + " until " + visualizeDate("endsOn",aPlannedTransaction.endsOn) + " with policy "+aPlannedTransaction.executionPolicy;
+}
+
+function visualizeAmountForEditing(aPlannedTransaction) {
+	var amount = visualizeAsTextField("cents",aPlannedTransaction.cents/100.0);
+	if ( aPlannedTransaction.currency == "EUR" ) {
+		amount = amount + "€";
+	}
+	return amount;
+}
+
+function visualizeForEditing(aPlannedTransaction) {
+	var WHITESPACE = "&nbsp;"
+	var title = visualizeAsTextField("title",aPlannedTransaction.narration);
+	var amount = visualizeAmountForEditing(aPlannedTransaction);
+	var schedule = visualizeScheduleForEditing(aPlannedTransaction);
+	return "<form>"+title + WHITESPACE + amount + WHITESPACE + schedule+"</form>";
+}
+
 function distributeJSon(data,selectedAccountEncoded) {
 	var selectedAccount = replaceHtmlUmlaute(selectedAccountEncoded);
 	// sort transactions according to in which account they'll be displayed
@@ -19,7 +47,7 @@ function distributeJSon(data,selectedAccountEncoded) {
 			$(this).children().append(function(index,html) {
 				var encodedTransactions = "";
 				myMap.get(title).forEach( function(element, index, array) {
-					encodedTransactions = encodedTransactions + "<li>"+element.narration+"</li>"
+					encodedTransactions = encodedTransactions + "<li>"+visualizeForEditing(element)+"</li>"
 				})
 				return encodedTransactions;
 			});
